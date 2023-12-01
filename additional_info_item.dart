8ebe9b4606ce901d394b77c19546e02a0f3c400f
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2023-12-01 15:36:52 +0530

    Create additional_info_item.dart

---
+++ additional_info_item.dart
@@ -0,0 +1,39 @@
+import "package:flutter/material.dart";
+
+class AdditionalInfoItem extends StatelessWidget {
+  final IconData icon;
+  final String temperature;
+  final String value;
+
+  const AdditionalInfoItem({
+    super.key,
+    required this.icon,
+    required this.temperature,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          size: 32,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          temperature,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          value,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
