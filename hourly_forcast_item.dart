df014b5b125750bb79eae7b5c0160174ad18c81e
Chinmay Wali <123446377+chinmaywali@users.noreply.github.com>
2023-12-01 15:37:34 +0530

    Create hourly_forcast_item.dart

---
+++ hourly_forcast_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class HourlyForecastItem extends StatelessWidget {
+  final String time;
+  final IconData icon;
+  final String temperature;
+
+  const HourlyForecastItem({
+    super.key,
+    required this.time,
+    required this.icon,
+    required this.temperature,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child: Container(
+        width: 100,
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          children: [
+            Text(
+              time,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Icon(
+              icon,
+              size: 32,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              temperature,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
